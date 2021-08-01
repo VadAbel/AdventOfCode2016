adf0cd5a232c59e890396bd693f2588bfb6bf79b
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-08-01 12:06:38 +0200

    Add Day22

---
+++ Elixir/lib/day22/day22.ex
@@ -0,0 +1,135 @@
+defmodule Aoc2016.Day22 do
+  @day "22"
+  @input_file "../inputs/day#{@day}.txt"
+
+  defmodule SavedState do
+    use Agent
+
+    def start_link(initial_value) do
+      {:ok, pid} = Agent.start_link(fn -> MapSet.new(initial_value) end)
+      pid
+    end
+
+    def member_and_add?(node_state, pid) do
+      Agent.get_and_update(pid, fn state ->
+        {MapSet.member?(state, node_state), MapSet.put(state, node_state)}
+      end)
+    end
+  end
+
+  def parse(input) do
+    input
+    |> Enum.drop(2)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def parse_line(line) do
+    [node, size, used, avail, _use_percent] = String.split(line, " ", trim: true)
+
+    %{
+      name: parse_node(node),
+      size: parse_size(size),
+      used: parse_size(used),
+      avail: parse_size(avail)
+    }
+  end
+
+  def parse_node(node_string) do
+    [_, "x" <> x, "y" <> y] =
+      node_string
+      |> String.split("/", trim: true)
+      |> List.last()
+      |> String.split("-", trim: true)
+
+    [x, y]
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  def parse_size(size_string) do
+    "T" <> size_reverse = String.reverse(size_string)
+
+    String.reverse(size_reverse)
+    |> String.to_integer()
+  end
+
+  def find_pair(nodes_list) do
+    for node_a <- nodes_list,
+        node_a.used > 0,
+        node_b <- nodes_list -- [node_a],
+        node_b.avail >= node_a.used,
+        do: {node_a, node_b}
+  end
+
+  def neighbor({x, y}) do
+    [{-1, 0}, {1, 0}, {0, -1}, {0, 1}]
+    |> Enum.map(fn {a_x, a_y} -> {a_x + x, a_y + y} end)
+  end
+
+  def process_part2(node_list, state_list, saved_state, turn) do
+    new_state_list =
+      for {empty_node, data_node} <- state_list,
+          new_empty <- neighbor(empty_node),
+          new_empty in node_list,
+          new_data = if(new_empty == data_node, do: empty_node, else: data_node),
+          not SavedState.member_and_add?({new_empty, new_data}, saved_state),
+          do: {new_empty, new_data}
+
+    if Enum.any?(new_state_list, &(elem(&1, 1) == {0, 0})),
+      do: turn,
+      else: process_part2(node_list, new_state_list, saved_state, turn + 1)
+  end
+
+  def solution1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> parse()
+    |> find_pair()
+    |> Enum.count()
+  end
+
+  def solution2(input) do
+    node_map =
+      input
+      |> String.split("\n", trim: true)
+      |> parse()
+
+    empty_node =
+      node_map
+      |> get_in([Access.filter(&(&1.used == 0))])
+      |> hd()
+
+    data_node =
+      node_map
+      |> get_in([Access.filter(&(&1.name |> elem(1) == 0))])
+      |> Enum.max_by(&elem(&1.name, 0))
+
+    node_state = [{empty_node.name, data_node.name}]
+
+    saved_state = SavedState.start_link(node_state)
+
+    node_map
+    |> get_in([Access.filter(&(&1.used <= empty_node.size)), :name])
+    |> process_part2(node_state, saved_state, 1)
+  end
+
+  @doc """
+  iex> Aoc2016.Day#{@day}.part1()
+  1043
+  """
+  def part1 do
+    File.read!(@input_file)
+    |> solution1
+    |> IO.inspect(label: "Day#{@day} Part1 result ")
+  end
+
+  @doc """
+  iex> Aoc2016.Day#{@day}.part2()
+  185
+  """
+  def part2 do
+    File.read!(@input_file)
+    |> solution2
+    |> IO.inspect(label: "Day#{@day} Part2 result ")
+  end
+end
---
+++ Elixir/test/day22/day22_test.exs
@@ -0,0 +1,18 @@
+defmodule Aoc2016Test.Day22Test do
+  use ExUnit.Case
+
+  # import Aoc2016.Day22
+
+  doctest Aoc2016.Day22
+
+  # @test_input """
+  # """
+
+  test "Day22 Test1" do
+    # assert Aoc2016.Day22.solution1(@test_input, ?a..?e) == "decab"
+  end
+
+  test "Day22 Test2" do
+    # assert Aoc2016.Day22.solution2("5") == 2
+  end
+end
